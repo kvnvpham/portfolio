b0b709684227ac222890bef084feaf3fdcf821de
Kevin Pham <kvnvpham@gmail.com>
2023-04-26 15:46:32 -0500

    Design portfolio

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ def home():
     return render_template("index.html", projects=data)
 
 
+@app.route("/<int:type_id>")
+def get_projects(type_id):
+    def select_project(category):
+        if category["id"] == type_id:
+            return True
+        return False
+
+    with open("data.json") as file:
+        data = json.load(file)
+
+        filtered_data = filter(select_project, data)
+        clean_data = list(filtered_data)
+    
+    return render_template("projects.html", data=clean_data[0])
+
+
 @app.context_processor
 def copyright():
     return {"year": date.today().year}
